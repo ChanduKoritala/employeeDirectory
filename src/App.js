6a6f31749875e669bed3b6fe67bb530cf7f7ad12
Chandu Koritala <chanduk0406@gmail.com>
2020-04-07 18:25:05 +0930

    created a method called handlesort

--- src/App.js
+++ src/App.js
@@ -10,15 +10,16 @@ class App extends Component {
   // Setting this.state.employee to the employee json array 
   state = {
     employees,
-    sort: ''
   };
+
+  
 
   filterEmployeeByLocation = (location = "adelaide") => {
     let filterLocation = employees.filter((employee) => {
       console.log(this);
       return employee.location.toLowerCase().includes(location)
     })
-    this.setState({employees: filterLocation})    
+    this.setState({ employees: filterLocation })
   }
 
   handleInput = (e) => {
@@ -26,19 +27,31 @@ class App extends Component {
     this.filterEmployeeByLocation(location);
   }
 
-  
-  
-  // Map over this.state.employee and render a EmployeeCard component for each employee object
-  render() {
-    return (
-      <Wrapper>
-        <Title>Employee Directory</Title>
-        <FilterBox handleInput= {this.handleInput}/>
-        <Sort handleSort= {this.handleSort}/>
-        {this.state.employees.map( employee => <EmployeeCard employee= {employee} />)}
-      </Wrapper>
-    );
-  }
-}
+  handleSort = (e) => {
+    let sortedEmployees = [...employees];
+    sortedEmployees.sort((a, b) => {
+      if (a.name < b.name) {
+        return -1;
+      }
+      if (a.name > b.name) {
+        return 1;
+      }
+      return 0;
+    })
 
-export default App;
+  }
+
+    // Map over this.state.employee and render a EmployeeCard component for each employee object
+    render() {
+      return (
+        <Wrapper>
+          <Title>Employee Directory</Title>
+          <FilterBox handleInput={this.handleInput} />
+          <Sort handleSort={this.handleSort} />
+          {this.state.employees.map(employee => <EmployeeCard employee={employee} />)}
+        </Wrapper>
+      );
+    }
+  }
+
+  export default App;
--- src/components/Sort/index.js
+++ src/components/Sort/index.js
@@ -4,8 +4,8 @@ import "./style.css";
 function Sort(props) {
     return (
         <select className= "select" defaultValue ="Sort" onChange= {props.handleSort}>
-        <option value= "Name">Name</option>
-        <option value= "Date Of Birth">DateOfBirth</option>
+        <option value= "Name">Sort by Ascending Order</option>
+        <option value= "Date Of Birth">Sort by Descending Order</option>
         </select>
     )
 };
